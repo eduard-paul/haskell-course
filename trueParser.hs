b6cdc7563589c5f7502eedf2f1869e9ccc523b16
Eduard <eduard.paul.94@gmail.com>
2015-04-25 15:41:03 +0300

    Swaping files

--- trueParser.hs
+++ trueParser.hs
@@ -1,109 +1,49 @@
-import Data.Char
+module Calculator
+    ( Expr
+    , parseExpr
+    , calcExpr
+    )
+    where
+    
+{- Выражение не очень удобно вычислять сразу при вводе, поэтому разумно 
+сохранить его в новый тип данных, которое представить в виде синтаксического дерева.
+Тип выражения параметризован типом чисел, которые в нём могут храниться.-}
+data Expr a
+    = Const a --константы
+    | Binary BinOp <...> --операторы
+    | Unary UnOp <...> --унарные операторы и функции
+    
+type BinOp :: a -> a -> a
+type UnOp :: <...>
+    
+{- Выражение можно парсить. Проще всего сделать это методом рекурсивного спуска.
+(https://ru.wikipedia.org/wiki/Метод_рекурсивного_спуска)
+-}
+parseExpr :: String -> Expr a
+parseExpr = undefined
+{- Разбор выражения можно сделать и любым другим методом, например, перегонкой через
+обратную польскую запись. -}
 
-data Operator = Plus | Minus | Times | Div
-    deriving (Show, Eq)
+{-Бонус 2: выражение также можно выводить.
+instance Show a => Show (Expr a) where
+    show (Const c) = show c
+    show <...>
+    
+Но для этого придётся хранить функции в символьном виде:
+data BinOp = Plus | <...>
+-}
+    
+{- Выражение нужно уметь вычислять. -}
+calcExpr :: Num a => Expr a -> a
+calcExpr (Const a) = a --константа вычисляется в её значение
+<...>
 
-data Token = TokOp Operator
-           | TokAssign
-           | TokLParen
-           | TokRParen
-           | TokIdent String
-           | TokNum Double
-           | TokEnd
-    deriving (Show, Eq)
-
-operator :: Char -> Operator
-operator c | c == '+' = Plus
-           | c == '-' = Minus
-           | c == '*' = Times
-           | c == '/' = Div
- 
-tokenize :: String -> [Token]
-tokenize [] = []
-tokenize (c : cs) 
-    | elem c "+-*/" = TokOp (operator c) : tokenize cs
-    | c == '='  = TokAssign : tokenize cs
-    | c == '('  = TokLParen : tokenize cs
-    | c == ')'  = TokRParen : tokenize cs
-    | isDigit c = number c cs
-    | isAlpha c = identifier c cs
-    | isSpace c = tokenize cs
-    | otherwise = error $ "Cannot tokenize " ++ [c]
-
-identifier :: Char -> String -> [Token]
-identifier c cs = let (name, cs') = span isAlphaNum cs in
-                  TokIdent (c:name) : tokenize cs'
-
-number :: Char -> String -> [Token]
-number c cs = 
-   let (digs, cs') = span isDigit cs in
-   TokNum (read (c : digs)) : tokenize cs'
-
----- parser ----
-
-data Tree = SumNode Operator Tree Tree
-          | ProdNode Operator Tree Tree
-          | AssignNode String Tree
-          | UnaryNode Operator Tree
-          | NumNode Double
-          | VarNode String
-    deriving Show
-
-lookAhead :: [Token] -> Token
-lookAhead [] = TokEnd
-lookAhead (t:ts) = t
-
-accept :: [Token] -> [Token]
-accept [] = error "Nothing to accept"
-accept (t:ts) = ts
-
-expression :: [Token] -> (Tree, [Token])
-expression toks = 
-   let (termTree, toks') = term toks
-   in
-      case lookAhead toks' of
-         (TokOp op) | elem op [Plus, Minus] -> 
-            let (exTree, toks'') = expression (accept toks') 
-            in (SumNode op termTree exTree, toks'')
-         TokAssign ->
-            case termTree of
-               VarNode str -> 
-                  let (exTree, toks'') = expression (accept toks') 
-                  in (AssignNode str exTree, toks'')
-               _ -> error "Only variables can be assigned to"
-         _ -> (termTree, toks')
-
-term :: [Token] -> (Tree, [Token])
-term toks = 
-   let (facTree, toks') = factor toks
-   in
-      case lookAhead toks' of
-         (TokOp op) | elem op [Times, Div] ->
-            let (termTree, toks'') = term (accept toks') 
-            in (ProdNode op facTree termTree, toks'')
-         _ -> (facTree, toks')
-
-factor :: [Token] -> (Tree, [Token])
-factor toks = 
-   case lookAhead toks of
-      (TokNum x)     -> (NumNode x, accept toks)
-      (TokIdent str) -> (VarNode str, accept toks)
-      (TokOp op) | elem op [Plus, Minus] -> 
-            let (facTree, toks') = factor (accept toks) 
-            in (UnaryNode op facTree, toks')
-      TokLParen      -> 
-         let (expTree, toks') = expression (accept toks)
-         in
-            if lookAhead toks' /= TokRParen 
-            then error "Missing right parenthesis"
-            else (expTree, accept toks')
-      _ -> error $ "Parse error on token: " ++ show toks
-
-parse :: [Token] -> Tree
-parse toks = let (tree, toks') = expression toks
-             in
-               if null toks' 
-               then tree
-               else error $ "Leftover tokens: " ++ show toks'
-
-main = (print . parse . tokenize) "x1 = -15 / (2 + x2)"
+{- Бонус 1: вычислять выражение с переменными.
+Для этого нужно добавить возможность хранить переменные в выражении.
+Переменная однозначно определяется именем:
+type Var = String
+Для вычисления нужно передавать значения его переменных:
+calcExpr :: Num a => [(Var, a)] -> Expr a -> a
+Или же выводить выражение недовычисленным (бонус 1c):
+calcExpr :: Num a => [(Var, a)] -> Expr a -> Expr a
+-}
